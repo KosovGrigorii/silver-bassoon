41b1c825cdd4e24fb79dcc7309bb7dae4d38a18e
Grigorij_Kosov <kosov.gr@mail.ru>
2020-11-30 20:24:45 +0500

    описание коммита

---
+++ Json.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Json._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var str = Console.ReadLine();
+            var sum = 0;
+            for(var i = 0; i < str.Length; i++)
+            {
+                var builder = new StringBuilder();
+                if (char.IsDigit(str[i]) || str[i] == '-')
+                {
+                    builder.Append(str[i]);
+                    i++;
+                    while (char.IsDigit(str[i]))
+                    {
+                        builder.Append(str[i]);
+                        i++;
+                    }
+                    sum += int.Parse(builder.ToString());
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
